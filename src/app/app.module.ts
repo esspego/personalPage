d1028072db31ad641960b94eddca23a44502d57e
Espe <ego.egarco01@gmail.com>
2020-09-07 18:07:47 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    purge: [],
+    theme: {
+        backgroundColor: theme => ({
+            'brown': '#D8C0A7',
+        }),
+        extend: {
+            height: {
+                '70vh': '70vh',
+                '30vh': '30vh'
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+}
